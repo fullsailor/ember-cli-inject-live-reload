1f1aba09a6884a273a4e411b7e1efc3084c7cc42
Andrew Smith <andrew.smith@intelitycorp.com>
2015-07-31 11:31:40 -0400

    Remove middleware, allow whole server to be set

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 'use strict';
 
 module.exports = {
-  name: 'live-reload-middleware',
+  name: 'live-reload-script-injector',
 
-  contentFor: function(type) {
-    var liveReloadScriptSrc = process.env.EMBER_CLI_INJECT_LIVE_RELOAD_SCRIPT_SRC;
+  contentFor: function(type, config) {
+    if (type !== 'head') { return; }
+    if (config.liveReload !== true) { return; }
 
-    if (liveReloadScriptSrc && type === 'head') {
-      return '<script src="' + liveReloadScriptSrc + '" type="text/javascript"></script>';
+    var src
+    if (config.liveReloadServer) {
+      src = config.liveReloadServer + '/livereload.js?snipver=1'
+    } else {
+      var scheme = options.ssl ? 'https://' : 'http://';
+      src = [scheme, liveReloadHost, ':', options.liveReloadPort, ].join('');
     }
-  },
-  serverMiddleware: function(config) {
-    var options = config.options;
 
-    if (options.liveReload !== true) { return; }
-
-    var liveReloadHost = options.liveReloadHost || options.host;
-
-    var scheme = options.ssl ? 'https://' : 'http://';
-    var src = [scheme, liveReloadHost, ':', options.liveReloadPort, '/livereload.js?snipver=1'].join('');
-
-    process.env.EMBER_CLI_INJECT_LIVE_RELOAD_SCRIPT_SRC = src;
+    if (src) {
+      return '<script src="' + src + '" type="text/javascript"></script>';
+    }
   }
 };
